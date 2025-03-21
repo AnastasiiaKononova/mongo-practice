835c340aa1865fc9174b5dec3c785d240f088a89
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-03-21 16:16:34 +0200

    Task resolve p.1

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+const Movie = require('./Movie');
+const db = require('../configs/mConfig.json');
+ 
+const CONFIG = db[process.env.NODE_ENV || "development"];
+
+const url = "mongodb+srv://nastya:Anastasiia123456789@cluster0.1mp7w.mongodb.net/mongo-welcome-db"
+//  const url = `mongodb://${CONFIG.host}:${CONFIG.port}/${CONFIG.database}`
+ 
+ mongoose.connect(url)
+ .catch((err) => {
+     console.log('connection failed');
+     process.exit(1);
+ })
+ 
+ module.exports = {
+     User,
+     Movie
+ }
