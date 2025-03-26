752ac6745cb2e75733aa037a61efaa382a4b2711
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-03-26 14:13:12 +0200

    postgres dump

--- pg-test.session.sql
+++ pg-test.session.sql
@@ -5,15 +5,28 @@ CREATE TABLE cats (
     favouriteFood text
 );
 
+INSERT INTO cats (name, age, color, favouriteFood) VALUES ('Murzik', 5, 'black', '[]')
+/*
+Імпорт з файлу
+\COPY cats FROM '/tmp/cats.csv' DELIMITER ',' CSV HEADER;
+
+Використання папки /tmp дозволяє серверу читати вміст файла для імпорта
+
+Еспорт до файлу
+\COPY movies TO '/tmp/movies.csv' DELIMITER ',' CSV HEADER;
+
+
+*/
+
+
+--- ДАМП БД - повний бекап, необхідний для відтворення аналогічної структури та даних в іншому місці або у випадку пошкодження початкової БД
+
+--pfm_2023_practice
 
 /*
- Імпорт з файлу
- \COPY cats FROM '/tmp/cats.csv' DELIMITER ',' CSV HEADER;
- 
- Використання папки /tmp дозволяє серверу читати вміст файла для імпорта
- 
- Еспорт до файлу
- \COPY movies TO '/tmp/movies.csv' DELIMITER ',' CSV HEADER;
- 
- 
- */
+Створення дампа - утіліта pg_dump
+
+Правцювати вона може тільки від імені користувача postgres в системі
+А цей користувач має доступ до запису\копіюванню даних тільки в папці /tmp
+
+pg_dump -d pfm_2023_practice -f '/tmp/pgDUMP.sql'
